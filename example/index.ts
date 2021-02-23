cff44b0b449da194ab7c37c46fd40568d7be9b40
avidcoder123 <umar.tli.edu@gmail.com>
2021-02-23 22:00:01 +0000

    Delete post route example

--- example/index.ts
+++ example/index.ts
@@ -8,10 +8,6 @@ Router.route('/:id', async function(ctx: HttpContext){
     return "Recieved param id: " + ctx.request.params.id as string
 })
 
-Router.post("/", async function(ctx: HttpContext){
-    return "POST is working!"
-})
-
 
 app.start({
     port: 8080
