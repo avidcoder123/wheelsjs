05f2f497fae366356aa0c7120043c5ae27009880
avidcoder123 <umar.tli.edu@gmail.com>
2021-04-26 16:29:48 +0000

    Bootstrap function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import StartupClass from "../contracts/startup"
+import "reflect-metadata"
+import { container, InjectionToken } from 'tsyringe'
 
-export function bootstrap <T extends StartupClass>() {
-
+export function bootstrap <T extends InjectionToken>(startup: T) {
+    let opts = container.resolve<T>(startup) as unknown as StartupClass
+    
 }
